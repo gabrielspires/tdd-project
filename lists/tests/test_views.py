6e6bbd705adab909afecda0e1abbc9a702028274
Gabriel Pires <gabrielhpires@gmail.com>
2021-03-03 23:35:00 -0300

    De volta ao teste funcional - PARTE 1

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -55,15 +55,12 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, "other list item 1")
         self.assertNotContains(response, "other list item 2")
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
+            f"/lists/{correct_list.id}/", data={"item_text": "A new item for an existing list"}
         )
 
         self.assertEqual(Item.objects.count(), 1)
@@ -76,10 +73,8 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            f"/lists/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
+            f"/lists/{correct_list.id}/", data={"item_text": "A new item for an existing list"}
         )
-
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
 
